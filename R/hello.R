055780eb09e4c7361321c2faeb3378b4d8ab243c
Cuihua Xia <xiacuihuar@gmail.com>
2025-08-31 23:12:32 +0800

    Update hello.R

--- R/hello.R
+++ R/hello.R
@@ -64,13 +64,12 @@ library(grid)  # 用于 unit()
 
 
 
-
 my_theme <- function(base_size = 12, scale = 1) {
   theme_bw(base_size = base_size) +
     theme(
       ## Layout
       panel.grid       = element_blank(),
-      panel.border     = element_rect(colour = "black", fill = NA, linewidth = 0.6 * scale),
+      panel.border     = element_rect(colour = "black", fill = NA, linewidth = 0.8 * scale),
       panel.background = element_blank(),
       plot.background  = element_blank(),
 
@@ -82,9 +81,9 @@ my_theme <- function(base_size = 12, scale = 1) {
 
       ## Axes
       axis.title       = element_text(size = rel(1.0 * scale), face = "bold", colour = "black"),
-      axis.text        = element_text(size = rel(0.85 * scale),               colour = "black"),
-      axis.line        = element_line(linewidth = 0.6 * scale, colour = "black"),
-      axis.ticks       = element_line(linewidth = 0.6 * scale),
+      axis.text        = element_text(size = rel(0.85 * scale), colour = "black"),
+      axis.line        = element_line(linewidth = 0.8 * scale, colour = "black"),
+      axis.ticks       = element_line(linewidth = 0.8 * scale),
       axis.ticks.length= unit(0.15 * scale, "lines"),
 
       ## Legend
@@ -93,8 +92,8 @@ my_theme <- function(base_size = 12, scale = 1) {
       legend.text       = element_text(size = rel(0.85 * scale)),
       legend.key        = element_blank(),
       legend.background = element_blank(),
-      legend.key.size   = unit(0.9 * scale, "lines"),
-
+      legend.key.size   = unit(1 * scale, "lines"),
+     
       ## Facet
       strip.text        = element_text(size = rel(1.05 * scale), face = "bold",
                                        margin = margin(5 * scale, 0, 5 * scale, 0)),
