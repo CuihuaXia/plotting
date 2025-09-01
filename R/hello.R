f46e9e2171cba94e311c71b4b26b14bb9547487d
Cuihua Xia <xiacuihuar@gmail.com>
2025-09-01 20:00:27 +0800

    Update hello.R

--- R/hello.R
+++ R/hello.R
@@ -65,46 +65,6 @@ library(grid)  # 用于 unit()
 
 
 
-# my_theme <- function(base_size = 12, scale = 1) {
-
-#   theme_bw(base_size = base_size) +
-#     theme(
-#       ## Layout
-#       panel.grid       = element_blank(),
-#       panel.border     = element_rect(colour = "black", fill = NA, linewidth = 0.5 * scale),
-#       panel.background = element_blank(),
-#       plot.background  = element_blank(),
-
-#       ## Title
-#       plot.title       = element_text(
-#         size = rel(1.3 * scale), face = "bold", hjust = 0.5,
-#         margin = margin(b = 8 * scale)
-#       ),
-
-#       ## Axes
-#       axis.title       = element_text(size = rel(1.0 * scale), face = "bold", colour = "black"),
-#       axis.text        = element_text(size = rel(0.85 * scale), colour = "black"),
-#       axis.line        = element_line(linewidth = 0.5 * scale, colour = "black"),
-#       axis.ticks       = element_line(linewidth = 0.5 * scale, colour = "black"),
-#       axis.ticks.length= unit(0.15 * scale, "lines"),
-
-#       ## Legend
-#       legend.position   = "right",
-#       legend.title      = element_text(size = rel(0.95 * scale), face = "bold"),
-#       legend.text       = element_text(size = rel(0.85 * scale)),
-#       legend.key        = element_blank(),
-#       legend.background = element_blank(),
-#       legend.key.size   = unit(1 * scale, "lines"),
-     
-#       ## Facet
-#       strip.text        = element_text(size = rel(1.05 * scale), face = "bold", margin = margin(5 * scale, 0, 5 * scale, 0)),
-#       # strip.background  = element_blank()
-#       strip.background  = element_rect(fill = "grey92", colour = "black", linetype = 'solid', linewidth = 0.8 * scale)
-#     )
-# }
-
-
-
 
 my_theme <- function(base_size = 12, scale = 1, frame = "panel") {
   frame <- match.arg(frame)
@@ -134,10 +94,10 @@ my_theme <- function(base_size = 12, scale = 1, frame = "panel") {
       legend.key.size   = unit(1 * scale, "lines"),
 
       ## Facet
-      strip.text        = element_text(size = rel(1.05 * scale), face = "bold", margin = margin(t = 5 * scale, r = 0, b = 2 * scale, l = 0)),
+      strip.text        = element_text(size = rel(1.05 * scale), face = "bold", margin = margin(t = 5 * scale, r = 0, b = 6 * scale, l = 0)),
       strip.background  = element_rect(fill = "grey92", colour = "black", linetype = "solid", linewidth = 0.5 * scale),
-      strip.switch.pad.grid = unit(2 * scale, "pt"),
-      strip.switch.pad.wrap = unit(2 * scale, "pt")
+      strip.switch.pad.grid = unit(6 * scale, "pt"),
+      strip.switch.pad.wrap = unit(6 * scale, "pt")
     )
 
   # 根据 frame 参数切换
