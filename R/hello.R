c55fc162c6be9525b6074ae4e1b3fb55e4d31940
Cuihua Xia <xiacuihuar@gmail.com>
2025-09-01 22:27:54 +0800

    Update hello.R

--- R/hello.R
+++ R/hello.R
@@ -77,8 +77,8 @@ my_theme <- function(base_size = 12, scale = 1, frame = c("panel", "axis", "oute
 
       ## 每个facet分面图的上下和左右间距
       strip.placement = "outside",
-      panel.spacing.y = unit(3 * scale, "pt"),
-      panel.spacing.x = unit(3 * scale, "pt"),
+      panel.spacing.y = unit(2 * scale, "pt"),
+      panel.spacing.x = unit(2 * scale, "pt"),
 
       ## Title
       plot.title       = element_text(size = rel(1.3 * scale), face = "bold", hjust = 0.5, margin = margin(b = 8 * scale)),
